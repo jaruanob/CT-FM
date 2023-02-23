32ab3b8792587b9924eebef63c00885e12b8f623
Ibrahim Hadzic <ibrahimhadzic45@gmail.com>
2023-02-23 15:10:15 -0500

    Reorganize, add MSD Spleed evaluation

--- transforms/monai_related.py
+++ transforms/monai_related.py
@@ -1,7 +1,8 @@
-from typing import Dict, List
+from typing import Callable, Dict, List, Union
 
 import torch
-from monai.transforms import Transform
+from monai.transforms import Transform, BoundingRect, SpatialCropd
+from monai.transforms.utils import  is_positive
 
 
 class ExtractFromDict(Transform):
@@ -11,3 +12,20 @@ class ExtractFromDict(Transform):
 
     def __call__(self, data: Dict[str, torch.Tensor]) -> List:
         return [data[key] for key in self.keys]
+
+
+class SpatialCropAroundBoundingRectd(Transform):
+    def __init__(
+            self, crop_keys: Union[str, List[str]], bbox_key: str, select_fn: Callable = is_positive
+        ) -> None:
+        super().__init__()
+        self.crop_keys = crop_keys
+        self.bbox_key = bbox_key
+        self.bbox_transform = BoundingRect(select_fn)
+
+    def __call__(self, data) -> Dict:
+        bbox = self.bbox_transform(data[self.bbox_key])
+        roi_start = bbox[:len(bbox) // 2]
+        roi_end = bbox[len(bbox) // 2:]
+        crop_transform = SpatialCropd(keys=self.crop_keys, roi_start=roi_start, roi_end=roi_end)
+        return crop_transform(data)
