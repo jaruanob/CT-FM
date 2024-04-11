ab55bc076d492e297a6d05c1df5faac0b448409e
Suraj Pai <bspai@bwh.harvard.edu>
2024-04-11 14:17:55 -0400

    Minor refactor

--- scripts/utils.py
+++ scripts/utils.py
@@ -34,3 +34,14 @@ def plot_3d_image(ret, cmap=None):
 
     plt.axis("off")
     plt.show()
+
+
+def process_radchest_labels(path):
+    import pandas as pd
+    df = pd.read_csv(path)
+    verified_labels = ["nodule", "mass", "opacity", "consolidation", "atelectasis", "pleural_effusion", "pneumothorax", "pericardial_effusion", "cardiomegaly"]
+    for label in verified_labels:
+        df[label] = df.filter(like=label).any(axis=1).astype(int)
+
+    df = df.sort_values(by=["NoteAcc_DEID"])
+    return df[verified_labels].values
