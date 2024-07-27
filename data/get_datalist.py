3e2098b12e63c2387d917ecb27c271ca17e31451
Suraj Pai <bspai@bwh.harvard.edu>
2024-07-26 20:49:37 -0400

    Simplify totalseg config structure

--- data/get_datalist.py
+++ data/get_datalist.py
@@ -1,31 +1,30 @@
 from pathlib import Path
 import pandas as pd
+from lighter.utils.misc import apply_fns
 
-def get_ts_class_indices(group="all"):
+
+def get_ts_class_indices(group="v1"):
     body_parts_ids = {
-        "organs": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
-        "vertebrae": [18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41],
-        "cardiac": [42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 104, 93],
-        "muscles": [82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103],
-        "ribs": [58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81],
+        "organs_v1": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
+        "vertebrae_v1": [18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41],
+        "cardiac_v1": [42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 104, 93],
+        "muscles_v1": [82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103],
+        "ribs_v1": [58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81],
         "merlin": [6, 5, 4, 3, 2, 1, 22, 32, 31, 30, 29, 28, 27, 26, 25, 21, 20, 19, 18, 7],
-        "all": list(range(1, 105)),
+        "v1": list(range(1, 105)),
         "v2": list(range(1, 118))
     }
 
     assert group in body_parts_ids.keys()
     return body_parts_ids[group] + [0]
 
-def get_ts_datalist(data_dir, split, percentage=100, filter_fn=lambda x: x):
+def get_ts_datalist(data_dir, percentage=100, filter_fn=[]):
     """
     Get the list of image and label paths for a given split.
     """
-    assert split in ["train", "val", "test"]
     data_dir = Path(data_dir)
-
     meta_df = pd.read_csv(data_dir / "meta.csv")
-    meta_df = meta_df[meta_df["split"] == split]
-    meta_df = filter_fn(meta_df)
+    meta_df = apply_fns(meta_df, filter_fn)
 
     ids = meta_df["image_id"].tolist()
 
@@ -35,7 +34,7 @@ def get_ts_datalist(data_dir, split, percentage=100, filter_fn=lambda x: x):
     images = images[: int(len(images) * percentage / 100)]
     labels = labels[: int(len(labels) * percentage / 100)]
 
-    print(f"Number of {split} images: {len(images)}")
+    print(f"Number of ]images: {len(images)}")
 
     return [
         {"image": image, "label": label, "id": id}
--- evaluation/scripts/totalseg.sh
+++ evaluation/scripts/totalseg.sh
@@ -1,64 +1,65 @@
 export WANDB_ENTITY=aim-harvard
+ct_fm_path="/mnt/data1/CT_FM/latest_fm_checkpoints/ct_fm_simclr_segresnetds_22_jul_2024.ckpt"
 
 
-######################### Decoder-only ############################
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_unet.yaml
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_segresnet.yaml
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/frameworks/ct_fm.yaml
+# # ######################### Decoder-only ############################
+# lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_unet.yaml --trainer#strategy=ddp_find_unused_parameters_true --vars#name="suprem_unet" --vars#project="totalseg" --vars#wandb_tag='$["representations"]'
+# lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_segresnet.yaml --trainer#strategy=ddp_find_unused_parameters_true --vars#name="suprem_segresnet" --vars#project="totalseg" --vars#wandb_tag='$["representations"]'
+# lighter fit --config=./evaluation/totalseg.yaml --trainer#strategy=ddp_find_unused_parameters_true --vars#name="ct_fm_segresnet" --vars#project="totalseg" --system#model#trunk#ckpt_path=$ct_fm_path --vars#wandb_tag='$["representations"]'
 
 
-######################### Few-shot train samples ############################
-# 20-shot 
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/random_init.yaml --trainer#callbacks=None --vars#percentage=2 
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_unet.yaml --trainer#callbacks=None --vars#percentage=2
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_segresnet.yaml --trainer#callbacks=None --vars#percentage=2
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/frameworks/ct_fm.yaml --trainer#callbacks=None --vars#percentage=2
+# # ######################### Few-shot train samples ############################
+# # 20-shot 
+lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/segresnetds.yaml --trainer#callbacks#0#until_epoch=0 --vars#percentage=2 --vars#name="baseline" --vars#project="totalseg" --vars#wandb_tag='$["few-shot", "20"]' 
+lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_unet.yaml --trainer#callbacks#0#until_epoch=0 --vars#percentage=2 --vars#name="suprem_unet" --vars#project="totalseg" --vars#wandb_tag='$["few-shot", "20"]'
+# lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_segresnet.yaml --trainer#callbacks#0#until_epoch=0 --vars#percentage=2
+lighter fit --config=./evaluation/totalseg.yaml --trainer#callbacks#0#until_epoch=0 --vars#percentage=2 --vars#name="ct_fm" --vars#project="totalseg" --system#model#trunk#ckpt_path=$ct_fm_path --vars#wandb_tag='$["few-shot", "20"]'
 
 
-# 10-shot
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/random_init.yaml --trainer#callbacks=None --vars#percentage=1 
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_unet.yaml --trainer#callbacks=None --vars#percentage=1
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_segresnet.yaml --trainer#callbacks=None --vars#percentage=1
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/frameworks/ct_fm.yaml --trainer#callbacks=None --vars#percentage=1
+# # 10-shot
+# lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/random_init.yaml --trainer#callbacks#0#until_epoch=0 --vars#percentage=1 --vars#name="baseline" --vars#project="totalseg" --vars#wandb_tag='$["few-shot", "10"]'
+# lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_unet.yaml --trainer#callbacks#0#until_epoch=0 --vars#percentage=1 --vars#name="suprem_unet" --vars#project="totalseg" --vars#wandb_tag='$["few-shot", "10"]'
+# # lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_segresnet.yaml --trainer#callbacks=None --vars#percentage=1 
+# lighter fit --config=./evaluation/totalseg.yaml,./evaluation/frameworks/ct_fm.yaml --trainer#callbacks#0#until_epoch=0 --vars#percentage=1 --vars#name="ct_fm" --vars#project="totalseg" --system#model#trunk#ckpt_path=$ct_fm_path --vars#wandb_tag='$["few-shot", "10"]'
 
-# 5-shot
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/random_init.yaml --trainer#callbacks=None --vars#percentage=0.5
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_unet.yaml --trainer#callbacks=None --vars#percentage=0.5
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_segresnet.yaml --trainer#callbacks=None --vars#percentage=0.5
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/frameworks/ct_fm.yaml --trainer#callbacks=None --vars#percentage=0.5
-
-
-
-######################### Few-shot annotation groups ############################
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/random_init.yaml --trainer#callbacks=None --vars#group="cardiac"
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_unet.yaml --trainer#callbacks=None --vars#group="cardiac"
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_segresnet.yaml --trainer#callbacks=None --vars#group="cardiac"
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/frameworks/ct_fm.yaml --trainer#callbacks=None --vars#group="cardiac"
+# # 5-shot
+# lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/random_init.yaml --trainer#callbacks#0#until_epoch=0 --vars#percentage=0.5 --vars#name="baseline" --vars#project="totalseg" --vars#wandb_tag='$["few-shot", "5"]'
+# lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_unet.yaml --trainer#callbacks#0#until_epoch=0 --vars#percentage=0.5 --vars#name="suprem_unet" --vars#project="totalseg" --vars#wandb_tag='$["few-shot", "5"]'
+# # lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_segresnet.yaml --trainer#callbacks#0#until_epoch=0 --vars#percentage=0.5
+# lighter fit --config=./evaluation/totalseg.yaml,./evaluation/frameworks/ct_fm.yaml --trainer#callbacks#0#until_epoch=0 --vars#percentage=0.5 --vars#name="ct_fm" --vars#project="totalseg" --system#model#trunk#ckpt_path=$ct_fm_path --vars#wandb_tag='$["few-shot", "5"]'
 
 
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_unet.yaml --trainer#callbacks=None --vars#group="organs"
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/random_init.yaml --trainer#callbacks=None --vars#group="organs"
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_segresnet.yaml --trainer#callbacks=None --vars#group="organs"
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/frameworks/ct_fm.yaml --trainer#callbacks=None --vars#group="organs"
 
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/random_init.yaml --trainer#callbacks=None --vars#group="muscles"
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_unet.yaml --trainer#callbacks=None --vars#group="muscles"
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_segresnet.yaml --trainer#callbacks=None --vars#group="muscles"
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/frameworks/ct_fm.yaml --trainer#callbacks=None --vars#group="muscles"
-
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/random_init.yaml --trainer#callbacks=None --vars#group="vertebra"
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_unet.yaml --trainer#callbacks=None --vars#group="vertebra"
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_segresnet.yaml --trainer#callbacks=None --vars#group="vertebra"
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/frameworks/ct_fm.yaml --trainer#callbacks=None --vars#group="vertebra"
-
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/random_init.yaml --trainer#callbacks=None --vars#group="muscles"
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_unet.yaml --trainer#callbacks=None --vars#group="muscles"
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_segresnet.yaml --trainer#callbacks=None --vars#group="muscles"
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/frameworks/ct_fm.yaml --trainer#callbacks=None --vars#group="muscles"
+# # ######################### Few-shot annotation groups ############################
+# # lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/random_init.yaml --trainer#callbacks=None --vars#group="cardiac"
+# # lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_unet.yaml --trainer#callbacks=None --vars#group="cardiac"
+# # # lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_segresnet.yaml --trainer#callbacks=None --vars#group="cardiac"
+# # lighter fit --config=./evaluation/totalseg.yaml,./evaluation/frameworks/ct_fm.yaml --trainer#callbacks=None --vars#group="cardiac"
 
 
-######################### Full ############################
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/random_init.yaml --trainer#callbacks=None
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_unet.yaml --trainer#callbacks=None
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_segresnet.yaml --trainer#callbacks=None
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/frameworks/ct_fm.yaml --trainer#callbacks=None
+# # lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_unet.yaml --trainer#callbacks=None --vars#group="organs"
+# # lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/random_init.yaml --trainer#callbacks=None --vars#group="organs"
+# # # lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_segresnet.yaml --trainer#callbacks=None --vars#group="organs"
+# # lighter fit --config=./evaluation/totalseg.yaml,./evaluation/frameworks/ct_fm.yaml --trainer#callbacks=None --vars#group="organs"
+
+# # lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/random_init.yaml --trainer#callbacks=None --vars#group="muscles"
+# # lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_unet.yaml --trainer#callbacks=None --vars#group="muscles"
+# # # lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_segresnet.yaml --trainer#callbacks=None --vars#group="muscles"
+# # lighter fit --config=./evaluation/totalseg.yaml,./evaluation/frameworks/ct_fm.yaml --trainer#callbacks=None --vars#group="muscles"
+
+# # lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/random_init.yaml --trainer#callbacks=None --vars#group="vertebra"
+# # lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_unet.yaml --trainer#callbacks=None --vars#group="vertebra"
+# # # lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_segresnet.yaml --trainer#callbacks=None --vars#group="vertebra"
+# # lighter fit --config=./evaluation/totalseg.yaml,./evaluation/frameworks/ct_fm.yaml --trainer#callbacks=None --vars#group="vertebra"
+
+# # lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/random_init.yaml --trainer#callbacks=None --vars#group="muscles"
+# # lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_unet.yaml --trainer#callbacks=None --vars#group="muscles"
+# # # lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_segresnet.yaml --trainer#callbacks=None --vars#group="muscles"
+# # lighter fit --config=./evaluation/totalseg.yaml,./evaluation/frameworks/ct_fm.yaml --trainer#callbacks=None --vars#group="muscles"
+
+
+# ######################### Full ############################
+# lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/random_init.yaml --trainer#callbacks#0#until_epoch=0
+# # lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_unet.yaml --trainer#callbacks#0#until_epoch=0
+# # lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_segresnet.yaml --trainer#callbacks#0#until_epoch=0
+# lighter fit --config=./evaluation/totalseg.yaml,./evaluation/frameworks/ct_fm.yaml --trainer#callbacks#0#until_epoch=0
