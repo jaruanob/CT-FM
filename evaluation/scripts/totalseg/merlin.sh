5f654ed7bc3a8bee20e69f6ec5baa45e1d100295
Suraj Pai <bspai@bwh.harvard.edu>
2024-08-17 13:52:31 -0400

    Update eval

--- evaluation/scripts/totalseg/merlin.sh
+++ evaluation/scripts/totalseg/merlin.sh
@@ -3,8 +3,13 @@
 ## These parameters organize how to label the wandb upload and where the model checkpoints are saved. 
 
 export WANDB_ENTITY=aim-harvard
-ct_fm_path="/mnt/data1/CT_FM/latest_fm_checkpoints/ct_fm_simclr_segresnetds_22_jul_2024.ckpt"
+# ct_fm_path="/mnt/data1/CT_FM/latest_fm_checkpoints/ct_fm_simclr_segresnetds_22_jul_2024.ckpt"
 
 # ######################### Groups - Merlin V2 ############################
 lighter fit --config=./evaluation/totalseg.yaml,./evaluation/overrides/totalseg_merlin.yaml,./evaluation/baselines/segresnetds.yaml --vars#name="baseline" --vars#project="totalseg" --vars#wandb_group='merlin_V2'
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/overrides/totalseg_merlin.yaml --system#model#trunk#ckpt_path=$ct_fm_path --vars#name="ct_fm" --vars#project="totalseg" --vars#wandb_group='merlin_V2'
+lighter fit --config=./evaluation/totalseg.yaml,./evaluation/overrides/totalseg_merlin.yaml --system#model#trunk#ckpt_path="/mnt/data1/CT_FM/latest_fm_checkpoints/original/epoch=449-step=225000-v1.ckpt" --vars#name="ct_fm" --vars#project="totalseg" --vars#wandb_group='merlin_V2'
+
+
+### Predict 
+# lighter predict --config=./evaluation/totalseg.yaml,./evaluation/overrides/totalseg_merlin.yaml,./evaluation/baselines/segresnetds.yaml,./evaluation/overrides/totalseg_predict_overrides.yaml  --vars#name="baseline" --vars#project="totalseg" --vars#wandb_group='merlin_V2'
+# lighter predict --config=./evaluation/totalseg.yaml,./evaluation/overrides/totalseg_merlin.yaml,./evaluation/overrides/totalseg_predict_overrides.yaml --system#model#trunk#ckpt_path="/mnt/data1/CT_FM/latest_fm_checkpoints/original/epoch=449-step=225000-v1.ckpt" --vars#name="ct_fm" --vars#project="totalseg" --vars#wandb_group='merlin_V2'
--- evaluation/scripts/totalseg/representations.sh
+++ evaluation/scripts/totalseg/representations.sh
@@ -3,9 +3,9 @@
 ## These parameters organize how to label the wandb upload and where the model checkpoints are saved. 
 
 export WANDB_ENTITY=aim-harvard
-ct_fm_path="/mnt/data1/CT_FM/latest_fm_checkpoints/ct_fm_simclr_segresnetds_22_jul_2024.ckpt"
+ct_fm_path="/mnt/data1/CT_FM/latest_fm_checkpoints/original/epoch=449-step=225000-v1.ckpt"
 
 # ######################### Decoder-only ############################
 lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_unet.yaml --trainer#strategy=ddp_find_unused_parameters_true --vars#name="suprem_unet" --vars#project="totalseg" --vars#wandb_group='representations'
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_segresnet.yaml --trainer#strategy=ddp_find_unused_parameters_true --vars#name="suprem_segresnet" --vars#project="totalseg" --vars#wandb_group='representations'
+lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/segresnet_vista3d.yaml --trainer#strategy=ddp_find_unused_parameters_true --vars#name="vista3d" --vars#project="totalseg" --vars#wandb_group='representations'
 lighter fit --config=./evaluation/totalseg.yaml --trainer#strategy=ddp_find_unused_parameters_true --vars#name="ct_fm_segresnet" --vars#project="totalseg" --system#model#trunk#ckpt_path=$ct_fm_path --vars#wandb_group='representations'
