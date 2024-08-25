fdabf4ad44d8cd1637574b0f9810d1ec36718a42
Ibrahim Hadzic <ibrahimhadzic45@gmail.com>
2024-08-25 17:53:24 -0400

    Standardize names of baseline yamls. Just like for vista3d or suprem, introduce CT FM specific architecture configs. Do not specify any architecture in the base config (e.g. totalseg.yaml). Instead use the baselines yamls for that. Updated all the .sh scripts to reflect the change. There are lits scripts that would not work anymore as they refer to "random_init.yaml" or "evaluation/frameworks/...yaml"

--- evaluation/scripts/totalseg/merlin.sh
+++ evaluation/scripts/totalseg/merlin.sh
@@ -6,10 +6,10 @@ export WANDB_ENTITY=aim-harvard
 # ct_fm_path="/mnt/data1/CT_FM/latest_fm_checkpoints/ct_fm_simclr_segresnetds_22_jul_2024.ckpt"
 
 # ######################### Groups - Merlin V2 ############################
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/overrides/totalseg_merlin.yaml,./evaluation/baselines/segresnetds.yaml --vars#name="baseline" --vars#project="totalseg" --vars#wandb_group='merlin_V2'
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/overrides/totalseg_merlin.yaml --system#model#trunk#ckpt_path="/mnt/data1/CT_FM/latest_fm_checkpoints/original/epoch=449-step=225000-v1.ckpt" --vars#name="ct_fm" --vars#project="totalseg" --vars#wandb_group='merlin_V2'
+lighter fit --config=./evaluation/totalseg.yaml,./evaluation/overrides/totalseg_merlin.yaml,./evaluation/baselines/segresnetds_random.yaml --vars#name="baseline" --vars#project="totalseg" --vars#wandb_group='merlin_V2'
+lighter fit --config=./evaluation/totalseg.yaml,./evaluation/overrides/totalseg_merlin.yaml,./evaluation/baselines/segresnetds_ctfm.yaml --system#model#trunk#ckpt_path="/mnt/data1/CT_FM/latest_fm_checkpoints/original/epoch=449-step=225000-v1.ckpt" --vars#name="ct_fm" --vars#project="totalseg" --vars#wandb_group='merlin_V2'
 
 
 ### Predict 
-# lighter predict --config=./evaluation/totalseg.yaml,./evaluation/overrides/totalseg_merlin.yaml,./evaluation/baselines/segresnetds.yaml,./evaluation/overrides/totalseg_predict_overrides.yaml  --vars#name="baseline" --vars#project="totalseg" --vars#wandb_group='merlin_V2'
-# lighter predict --config=./evaluation/totalseg.yaml,./evaluation/overrides/totalseg_merlin.yaml,./evaluation/overrides/totalseg_predict_overrides.yaml --system#model#trunk#ckpt_path="/mnt/data1/CT_FM/latest_fm_checkpoints/original/epoch=449-step=225000-v1.ckpt" --vars#name="ct_fm" --vars#project="totalseg" --vars#wandb_group='merlin_V2'
+# lighter predict --config=./evaluation/totalseg.yaml,./evaluation/overrides/totalseg_merlin.yaml,./evaluation/baselines/segresnetds_random.yaml,./evaluation/overrides/totalseg_predict_overrides.yaml  --vars#name="baseline" --vars#project="totalseg" --vars#wandb_group='merlin_V2'
+# lighter predict --config=./evaluation/totalseg.yaml,./evaluation/overrides/totalseg_merlin.yaml,./evaluation/baselines/segresnetds_ctfm.yaml,./evaluation/overrides/totalseg_predict_overrides.yaml --system#model#trunk#ckpt_path="/mnt/data1/CT_FM/latest_fm_checkpoints/original/epoch=449-step=225000-v1.ckpt" --vars#name="ct_fm" --vars#project="totalseg" --vars#wandb_group='merlin_V2'
