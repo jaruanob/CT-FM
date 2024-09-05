a1ff147cc2a91bacf1577b8ca40a90e2b1507fcf
Suraj Pai <bspai@bwh.harvard.edu>
2024-09-04 20:18:01 -0400

    Update fulltune

--- evaluation/scripts/totalseg/fulltune.sh
+++ evaluation/scripts/totalseg/fulltune.sh
@@ -3,9 +3,24 @@
 ## These parameters organize how to label the wandb upload and where the model checkpoints are saved. 
 
 export WANDB_ENTITY=aim-harvard
-ct_fm_path="/mnt/data1/CT_FM/latest_fm_checkpoints/original/epoch=129-step=65000-v1.ckpt"
+ct_fm_path="/mnt/data1/CT_FM/latest_fm_checkpoints/original/epoch=449-step=225000-v1.ckpt"
 
 # ######################### Full - V2 ############################
 lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/segresnetds.yaml --trainer#callbacks#0#until_epoch=0 --vars#group="v2" --vars#name="baseline" --vars#project="totalseg" --vars#wandb_group="fulltune_v2"
-lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_unet.yaml --trainer#callbacks#0#until_epoch=0 --vars#group="v2" --vars#name="suprem_unet" --vars#project="totalseg" --vars#wandb_group="fulltune_v2"
-lighter fit --config=./evaluation/totalseg.yaml --trainer#callbacks#0#until_epoch=0 --system#model#trunk#ckpt_path=$ct_fm_path --vars#name="ct_fm" --vars#project="totalseg" --vars#wandb_group="fulltune_v2"
+# lighter fit --config=./evaluation/totalseg.yaml --trainer#callbacks#0#until_epoch=0 --system#model#trunk#ckpt_path=$ct_fm_path --vars#name="ct_fm" --vars#project="totalseg" --vars#wandb_group="fulltune_v2"
+# lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_unet.yaml --trainer#callbacks#0#until_epoch=0 --vars#group="v2" --vars#name="suprem_unet" --vars#project="totalseg" --vars#wandb_group="fulltune_v2"
+# lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_segresnet.yaml --trainer#callbacks#0#until_epoch=0 --vars#group="v2" --vars#name="suprem_segresnet" --vars#project="totalseg" --vars#wandb_group="fulltune_v2"
+
+
+# ######################### Half - V2 ############################
+# lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/segresnetds.yaml --trainer#callbacks#0#until_epoch=0 --vars#group="v2" --vars#name="baseline" --vars#project="totalseg" --vars#wandb_group="fulltune_v2_50" --vars#percentage=50
+# lighter fit --config=./evaluation/totalseg.yaml --trainer#callbacks#0#until_epoch=0 --system#model#trunk#ckpt_path=$ct_fm_path --vars#name="ct_fm" --vars#project="totalseg" --vars#wandb_group="fulltune_v2_50" --vars#percentage=50
+# lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_unet.yaml --trainer#callbacks#0#until_epoch=0 --vars#group="v2" --vars#name="suprem_unet" --vars#project="totalseg" --vars#wandb_group="fulltune_v2_50" --vars#percentage=50
+# # lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_segresnet.yaml --trainer#callbacks#0#until_epoch=0 --vars#group="v2" --vars#name="suprem_segresnet" --vars#project="totalseg" --vars#wandb_group="fulltune_v2_50" --vars#percentage=50
+
+
+# # ######################### Quarter - V2 ############################
+# lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/segresnetds.yaml --trainer#callbacks#0#until_epoch=0 --vars#group="v2" --vars#name="baseline" --vars#project="totalseg" --vars#wandb_group="fulltune_v2_25" --vars#percentage=25
+# lighter fit --config=./evaluation/totalseg.yaml --trainer#callbacks#0#until_epoch=0 --system#model#trunk#ckpt_path=$ct_fm_path --vars#name="ct_fm" --vars#project="totalseg" --vars#wandb_group="fulltune_v2_25" --vars#percentage=25
+# lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_unet.yaml --trainer#callbacks#0#until_epoch=0 --vars#group="v2" --vars#name="suprem_unet" --vars#project="totalseg" --vars#wandb_group="fulltune_v2_25" --vars#percentage=25
+# # lighter fit --config=./evaluation/totalseg.yaml,./evaluation/baselines/suprem_segresnet.yaml --trainer#callbacks#0#until_epoch=0 --vars#group="v2" --vars#name="suprem_segresnet" --vars#project="totalseg" --vars#wandb_group="fulltune_v2_25" --vars#percentage=25
